5e6fd9a133111fbbda16017665fa9b01b330ceca
Serhii Ovchinnikov <50240729+ovchinnikovs@users.noreply.github.com>
2019-07-26 13:00:26 +0300

    Updates

---
+++ precache-manifest.33744772f61ee965a4cecdbf37e26e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589e6aee2ff69a3a07084c9d3e6704f6",
+    "url": "/React-counter/index.html"
+  },
+  {
+    "revision": "9ee9b9763d93ba86818f",
+    "url": "/React-counter/static/css/main.64e6e6f5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/React-counter/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9ee9b9763d93ba86818f",
+    "url": "/React-counter/static/js/main.b2b04989.chunk.js"
+  },
+  {
+    "revision": "500b7ad8a9505f46b8a9",
+    "url": "/React-counter/static/js/runtime~main.caa3acff.js"
+  }
+]);
